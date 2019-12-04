4cf5aa4e401acc55f57f3f3cfacc1d55d9964f97
Manav Bhojak <ManavBhojak@user3-227-207.wireless.utoronto.ca>
2019-12-04 18:50:24 -0500

    Add get_shot_coordinates method

--- src/Game.py
+++ src/Game.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import *
-from Player import Player
+from src.Player import Player
 import pygame
 
 
@@ -10,7 +10,9 @@ class Game:
 
     === Public Attributes ===
     player1:
-        The player that goes first.
+        The player that goes first; is a human player.
+    player2:
+        The player that goes second; is a CPU player.
     winner:
         The winner of the game, none if there are no winners yet.
     turn:
@@ -19,27 +21,32 @@ class Game:
         Whether this game is in progress or not.
     """
     player1: Player
+    player2: Player
     winner: Optional[Player]
     turn: Player
     running: bool
 
-    def __init__(self, player1: Player) -> None:
+    def __init__(self, player1: Player, player2: Player) -> None:
         """
         Initializes a game of ShootyBoats.
         """
         self.player1 = player1
+        self.player2 = player2
         self.winner = None  # no winners when initialized
         self.turn = player1  # player1 starts off the game, so they get 1st turn
         self.running = False  # set the game to "off"
 
-    def shoot_move(self) -> bool:
+    def get_shot_coordinates(self) -> tuple:
         """
         Have the player who's turn it is make a "shoot" move. Return True if the
         move was made, False otherwise.
         """
-        # TODO
+        if self.turn is self.player1:
+            return self.player1.ask_for_coordinates()
+        return self.player2.get_random_coordinates()
 
-    def place_move(self) -> bool:
+
+    def place(self) -> bool:
         """
         Have the player who's turn it is, make a "place_ship" move. Return True
         if the ship was placed, False otherwise.
@@ -57,7 +64,7 @@ class Game:
         else:
             return False
 
-    def start_ship_placing_stage(self) -> None:
+    def run_ship_placing_stage(self) -> None:
         """
         This runs the 1st stage of the game where both players place their ships
         on their ship board.
@@ -75,6 +82,8 @@ class Game:
         """
         Runs this game of ShootyBoats.
         """
+        self.run_ship_placing_stage()
+        self.run_battle_stage
 
 
     def end_game(self) -> None:
