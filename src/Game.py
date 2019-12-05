318d62c378323ec09fd0ca8a2374fc94f6880bfd
Manav Bhojak <ManavBhojak@user3-227-207.wireless.utoronto.ca>
2019-12-04 20:52:21 -0500

    Format Game class

--- src/Game.py
+++ src/Game.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
-from typing import *
 from src.Player import Player
-
 
 
 class Game:
@@ -13,8 +11,6 @@ class Game:
         The player that goes first; is a human player.
     player2:
         The player that goes second; is a CPU player.
-    winner:
-        The winner of the game, none if there are no winners yet.
     turn:
         The player who's turn it is.
     running:
@@ -22,7 +18,6 @@ class Game:
     """
     player1: Player
     player2: Player
-    winner: Optional[Player]
     turn: Player
     running: bool
 
@@ -34,7 +29,6 @@ class Game:
         self.player2 = player2
         self.winner = None  # no winners when initialized
         self.turn = player1  # player1 starts off the game, so they get 1st turn
-       # self.running = False  # set the game to "off"
 
     def get_shot_coordinates(self) -> tuple:
         """
@@ -45,31 +39,13 @@ class Game:
             return self.player1.select_target()
         return self.player2.get_random_coordinates()
 
-    def place(self):
+    def run_ship_placing_stage(self) -> None:
         """
         Have the player who's turn it is, make a "place_ship" move. Return True
         if the ship was placed, False otherwise.
         """
         self.player1.place_ships()
         self.player2.place_ships_randomly()
-
-    def is_game_over(self) -> bool:
-        """
-        Return True is game is over, false otherwise.
-        """
-        if self.player1.enemy_ships_sunk == 5 or \
-                self.player2.enemy_ships_sunk == 5:
-            self.end_game()
-            return True
-        else:
-            return False
-
-    def run_ship_placing_stage(self) -> None:
-        """
-        This runs the 1st stage of the game where both players place their ships
-        on their ship board.
-        """
-        self.place()
 
     def run_battle_stage(self) -> None:
         """
@@ -79,12 +55,20 @@ class Game:
         while self.player1.enemy_ships_sunk != 5 or \
                 self.player1.enemy_ships_sunk != 5:
 
+            # have the human make a move and print the boards so human can see
             if self.turn is self.player1:
                 print(self.player1.board.get_player_board_string())
                 print(self.player2.board.get_target_board_string())
                 self.player1.make_move()
 
+            # if its not player1's move, have the random player move
             self.player2.make_random_move()
+
+        # print winner
+        if self.player1.enemy_ships_sunk == 5:
+            print("Player1 has won")
+        else:
+            print("Player2 has won")
 
     def run_game(self) -> None:
         """
